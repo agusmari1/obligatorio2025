e45827183497482b7d7f6c07b001d1a0c9e665e1
pilarnoguera6 <pnoguera1@correo.um.edu.uy>
2025-05-24 12:58:18 -0300

    avance main

--- main.py/main_manu.py
+++ main.py/main_manu.py
@@ -1,3 +1,56 @@
-from entities.pieza import Pieza
-from entities.requerimiento import Requerimiento
-from entities.maquina import Maquina
+
+menu = [["1.Registrar:"],
+        ["           1.Pieza"],
+        ["           2.Maquina"],
+        ["           3.Cliente"],
+        ["           4.Pedido"],
+        ["           5.Reposicion"],
+        ["           6.Salir"],
+        ["2. Listar:"],
+        ["           1.Clientes"],
+        ["           2.Pedidos"],
+        ["           3.Maquinas"],
+        ["           4.Piezas"],
+        ["           5.Contabilidad"],
+        ["           6.Salir"],
+        ["3.Salir del Sistema"]]
+
+registrar = [["Registrar:"],
+        ["           1.Pieza"],
+        ["           2.Maquina"],
+        ["           3.Cliente"],
+        ["           4.Pedido"],
+        ["           5.Reposicion"],
+        ["           6.Salir"]]
+
+
+listar =[["Listar:"],
+        ["           1.Clientes"],
+        ["           2.Pedidos"],
+        ["           3.Maquinas"],
+        ["           4.Piezas"],
+        ["           5.Contabilidad"],
+        ["           6.Salir"]]
+for linea in menu:
+    print(linea[0])
+
+opcion1_2_3= int(input("Ingrese la opcion que desea ejecutar:registrar, listar o salir:"))
+
+def opcion_Novalida (opcion1_2_3):
+    if (opcion1_2_3 <1) or (opcion1_2_3>3):
+        return True
+    else:
+        return False
+    
+while opcion_Novalida (opcion1_2_3):
+    opcion1_2_3 = int (input ("Ingreselos valores1,2 o 3: "))
+
+if opcion1_2_3 == 1:
+    for linea in registrar:
+        print (linea[0])
+    opcion = int(input("Ingrese el numero de lo que desea registrar"))
+    
+
+    
+
+
