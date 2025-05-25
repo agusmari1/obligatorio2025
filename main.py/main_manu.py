856f4dc70b54deae73b78a9f0c41b639cad6e7ad
pilarnoguera6 <pnoguera1@correo.um.edu.uy>
2025-05-25 13:11:50 -0300

    main

--- main.py/main_manu.py
+++ main.py/main_manu.py
@@ -1,5 +1,12 @@
+# from cliente import ClienteParticular, Empresa
+# from maquina import Maquina
+# from pedido import Pedido
+# from pieza import Pieza
+# from reposicion import Reposicion
+# from requerimiento import Requerimiento
+# from sistema import Sistema
 
-menu = [["1.Registrar:"],
+menu_principal = [["1.Registrar:"],
         ["           1.Pieza"],
         ["           2.Maquina"],
         ["           3.Cliente"],
@@ -15,40 +22,55 @@ menu = [["1.Registrar:"],
         ["           6.Salir"],
         ["3.Salir del Sistema"]]
 
-registrar = [["Registrar:"],
-        ["           1.Pieza"],
-        ["           2.Maquina"],
-        ["           3.Cliente"],
-        ["           4.Pedido"],
-        ["           5.Reposicion"],
-        ["           6.Salir"]]
+menu_registrar = [["Registrar:"],
+        ["               1.Pieza"],
+        ["               2.Maquina"],
+        ["               3.Cliente"],
+        ["               4.Pedido"],
+        ["               5.Reposicion"],
+        ["               6.Salir"]]
 
 
-listar =[["Listar:"],
+menu_listar =[["Listar:"],
         ["           1.Clientes"],
         ["           2.Pedidos"],
         ["           3.Maquinas"],
         ["           4.Piezas"],
         ["           5.Contabilidad"],
         ["           6.Salir"]]
-for linea in menu:
+
+
+for linea in menu_principal:
     print(linea[0])
+
+print ("-----------------------------")
 
 opcion1_2_3= int(input("Ingrese la opcion que desea ejecutar:registrar, listar o salir:"))
 
-def opcion_Novalida (opcion1_2_3):
-    if (opcion1_2_3 <1) or (opcion1_2_3>3):
+if opcion1_2_3 == 1:
+    opcion_registrar ()
+elif opcion1_2_3 == 2:
+    listar ()
+elif opcion1_2_3 == 3:
+    print ("Saliste del sistema")
+    
+def opcion_NOvalida (opcion1_2_3):
+    if (opcion1_2_3<1) or (opcion1_2_3>3):
         return True
     else:
         return False
     
-while opcion_Novalida (opcion1_2_3):
-    opcion1_2_3 = int (input ("Ingreselos valores1,2 o 3: "))
+while opcion_NOvalida(opcion1_2_3):
+    print ("El valor ingresado no es valido")
+    opcion1_2_3 = int(input("Ingrese los valores1,2 o 3: "))
 
-if opcion1_2_3 == 1:
-    for linea in registrar:
+def opcion_registrar ():
+    for linea in menu_registrar:
         print (linea[0])
-    opcion = int(input("Ingrese el numero de lo que desea registrar"))
+
+
+
+
     
 
     
