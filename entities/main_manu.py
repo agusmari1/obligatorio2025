5459175de13a32d40629654428009fb2489e5127
larastirling <larastirlingterra29@gmail.com>
2025-05-27 10:39:24 -0300

    Update main_manu.py

--- entities/main_manu.py
+++ entities/main_manu.py
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
- 
-=======
-from entities. import Sistema
->>>>>>> a6719dc95bbe4171f8d7185d6bec59b6d7bd7820
-from entities.maquina import Maquina
-from entities.pieza import Pieza
+from sistema import Sistema
+from cliente import Cliente, ClienteParticular, Empresa
+from pedido import Pedido
+from requerimientos import Requerimiento
+from reposicion import Reposicion
+from maquina import Maquina
+from pieza import Pieza
 from exceptions.exceptionClienteYaexiste import ClienteRepetido
+from exceptions.exceptionPiezaYaExiste import PiezaRepetida
+
 sistema = Sistema() #crear una instancia para poder usar las funciones 
 #por que en este caso si y en los otros que llamamos a otras clases no?
 
@@ -66,15 +68,19 @@ def opcion_registrar (menu_registrar):
 #REGISTRAR UNA PIEZA 
     if objetoAregistrar == 1:
         print ("Se va a registrar una nueva pieza")
-        
-        try:
-            descripcion = input("Ingrese la descrpcion de la pieza")
-            for pieza in sistema._piezas:
-                if pieza.descripcion==pieza:
-                    raise PiezaRepetida()
+        while True:
+            try:
+                descripcion = input("Ingrese la descrpcion de la pieza")
+                for pieza in sistema._piezas:
+                    if pieza.descripcion==descripcion:
+                        raise PiezaRepetida()
+                    if descripcion==" ":
+                        raise ValueError() #la descripcion no puede ser vacia 
                 break
-        except PiezaRepetida as e:
-            print(e)
+            except PiezaRepetida as e:
+                print(e)
+            except ValueError as ve:
+                print(ve)
             
         costo_adquisicion= float(input("ingrese el costo de adquisición de la pieza"))
         unidades_en_lote= int(input("ingrese la cantidad de unidades de la pieza"))
@@ -116,13 +122,6 @@ def opcion_registrar (menu_registrar):
 
                         sistema.registro_maquina(nueva_maquina)
 
-
-
-
-                                   
-            
-
-
 # REGISTRAR UN CLIENTE
     elif objetoAregistrar ==3:
         print ("Se va a registrar un cliente")
@@ -131,21 +130,19 @@ def opcion_registrar (menu_registrar):
         telefono=input("seleccione el telefono")
         correo_electrónico=input("indique el correo electrónico")
         if tipo_cliente=="particular":
-            cedula=input("ingrese la cedula")
-            nuevo_cliente= ClienteParticular(nombre,telefono,correo_electrónico,cedula)
-
-        else:
-            pagina_web=input("ingrese la pagina web")
-            RUT=input("ingrese el rut")
-            nuevo_cliente=Empresa(nombre,telefono,correo_electrónico,RUT,pagina_web)
-
+            while True:
+                try:
+                    cedula=input("ingrese la cedula")
+                    for cliente in sistema._cliente:
+                        if isinstance(cliente,ClienteParticular)==True and cliente.cedula==cedula:
+                            raise ClienteRepetido
+                    break
                 except ClienteRepetido as e:
                     print (e)
-
-
-            nuevo_cliente=ClienteParticular(nombre,telefono,correo_electrónico,cedula)
+            
+            nuevo_cliente= ClienteParticular(nombre,telefono,correo_electrónico,cedula)
             sistema.registro_clientes(nuevo_cliente)
-                        
+
         else:
             while True:
                 try:
@@ -157,16 +154,10 @@ def opcion_registrar (menu_registrar):
                     
                 except ClienteRepetido as e:
                     print (e)
-            
-
             pagina_web=input("ingrese la pagina web")
             nuevo_cliente=Empresa(nombre,telefono,correo_electrónico,RUT,pagina_web)
             sistema.registro_clientes(nuevo_cliente)
-            
-            
-            
-
-        
+       
 
 # REGISTRAR UN PEDIDO
     elif objetoAregistrar == 4:
